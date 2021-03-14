caa2d1c42ba2eb7e321e7f521cbaf898ac6d13d9
DeniseLuiz <denisesantos.luiz@usp.br>
2021-03-13 23:49:13 -0300

    first project

---
+++ Solution other classes/AprendendoCSharpy/AprendendoCSharpy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoCSharpy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo! Projeto no Visual Studio");
+            Console.ReadLine();
+        }
+    }
+}
